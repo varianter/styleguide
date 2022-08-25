24d166048a39f9ab82a626be13ab63380c29cbd8
Jakob Kielland <jakob-ek@hotmail.com>
2022-08-25 13:53:17 +0200

    continue work on component, rough mvp done

--- packages/components/src/decorative-boxes/index.tsx
+++ packages/components/src/decorative-boxes/index.tsx
@@ -1,16 +1,16 @@
 import style from './style.module.css';
 
 interface BoxPosition {
-  top?: string;
-  left?: string;
-  bottom?: string;
-  right?: string;
+  top?: number;
+  left?: number;
+  bottom?: number;
+  right?: number;
 }
 
 export interface DecorativeBoxesProps
   extends React.ComponentPropsWithoutRef<any> {
   children: JSX.Element;
-  boxSize?: string;
+  boxSize?: number;
   box1Position?: BoxPosition;
   box2Position?: BoxPosition;
   box1Color?: string;
@@ -18,20 +18,37 @@ export interface DecorativeBoxesProps
 }
 
 /*
-Uncomplete todo-list:
+Incomplete todo-list:
 TODO: Implement random default colors
+TODO: Investigate if it is easier/more consistent
+ to just pass bottom/right as styling instead of 
+ translating to top/left every time
 */
 
 const DecorativeBoxes: React.FC<DecorativeBoxesProps> = ({
   children,
-  boxSize = '60%',
+  boxSize = 60,
   box1Position,
   box2Position,
   box1Color = 'var(--color-secondary2__tint4)',
   box2Color = 'var(--color-secondary1__tint4)',
 }) => {
-  const box1ComputedPositions = computePositions(boxSize, box1Position);
-  const box2ComputedPositions = computePositions(boxSize, box2Position);
+  const randomPositions = createRandomPosition(boxSize);
+  const box1ComputedPositions = getPosition(
+    boxSize,
+    randomPositions[0],
+    box1Position
+  );
+  const box2ComputedPositions = getPosition(
+    boxSize,
+    randomPositions[1],
+    box2Position
+  );
+
+  console.log('Box1 position:');
+  console.log(box1ComputedPositions);
+  console.log('Box2 position:');
+  console.log(box2ComputedPositions);
 
   return (
     <div className={style['main-container']}>
@@ -61,40 +78,124 @@ const DecorativeBoxes: React.FC<DecorativeBoxesProps> = ({
 export default DecorativeBoxes;
 
 function getInlineStyle(
-  boxSize: string,
+  boxSize: number,
   color: string,
-  positionTop: string,
-  positionLeft: string
+  positionTop: number,
+  positionLeft: number
 ) {
   return {
-    width: boxSize,
+    width: boxSize + '%',
     'background-color': color,
-    left: positionTop,
-    top: positionLeft,
+    left: positionTop + '%',
+    top: positionLeft + '%',
   };
 }
 
-function computePositions(boxSize: string, rawPositions?: BoxPosition) {
-  return {
-    top: '10%',
-    left: '0',
-  };
+function getPosition(
+  boxSize: number,
+  randomPosition: BoxPosition,
+  rawPosition?: BoxPosition
+) {
+  if (positionInfoIsSufficient(rawPosition)) {
+    return convertPosition(boxSize, rawPosition);
+  } else {
+    return convertPosition(boxSize, randomPosition);
+  }
 }
 
-function createRandomPositions(boxSize: string) {
-  /* Sections ensure boxes aren't generated too close to each other
-  Sections:
+/* Converts position values from uncertain top/right/bottom/left-format 
+to a guaranteed top/left-format */
+function convertPosition(boxSize: number, rawPosition?: BoxPosition) {
+  if (positionInfoIsSufficient(rawPosition)) {
+    // if-statement should guarantee rawPosition exists and has sufficient info
+    // I imagine there is a better way to do this..?
+    return {
+      top:
+        rawPosition!.top !== undefined
+          ? rawPosition!.top
+          : 100 - rawPosition!.bottom! - boxSize,
+      left:
+        rawPosition!.left !== undefined
+          ? rawPosition!.left
+          : 100 - rawPosition!.right! - boxSize,
+    };
+  } else {
+    return {
+      top: 0,
+      left: 0,
+    };
+  }
+}
+
+function createRandomPosition(boxSize: number) {
+  // Sections ensure boxes aren't generated too close to each other
+  const box1Section = randInt(8);
+  const box2Section = (box1Section + 2 + randInt(5)) % 8;
+  console.log(
+    'Box1 section: ' + box1Section + ', box2 section: ' + box2Section
+  );
+
+  return [
+    createPositionFromSection(box1Section, boxSize),
+    createPositionFromSection(box2Section, boxSize),
+  ];
+}
+
+function createPositionFromSection(section: number, boxSize: number) {
+  let rawPosition: BoxPosition = {};
+  const distanceFromCorner = randInt(50 - boxSize / 2);
+  /*  Sections:
     0 1
    7⌜ ⌝2
    6⌞ ⌟3
     5 4
   */
-  const box1Section = randomInteger(8);
-  const box2Section = (box1Section + 2 + randomInteger(5)) % 8;
-
-  //TODO: returns coordinates in the given section for each box
+  switch (section % 8) {
+    case 0:
+      rawPosition.top = 0;
+      rawPosition.left = distanceFromCorner;
+      break;
+    case 1:
+      rawPosition.top = 0;
+      rawPosition.right = distanceFromCorner;
+      break;
+    case 2:
+      rawPosition.top = distanceFromCorner;
+      rawPosition.right = 0;
+      break;
+    case 3:
+      rawPosition.bottom = distanceFromCorner;
+      rawPosition.right = 0;
+      break;
+    case 4:
+      rawPosition.bottom = 0;
+      rawPosition.right = distanceFromCorner;
+      break;
+    case 5:
+      rawPosition.bottom = 0;
+      rawPosition.left = distanceFromCorner;
+      break;
+    case 6:
+      rawPosition.bottom = distanceFromCorner;
+      rawPosition.left = 0;
+      break;
+    case 7:
+    default:
+      rawPosition.top = distanceFromCorner;
+      rawPosition.left = 0;
+      break;
+  }
+  return rawPosition;
 }
 
-function randomInteger(max: number) {
+function positionInfoIsSufficient(rawPosition?: BoxPosition) {
+  return (
+    rawPosition !== undefined &&
+    (rawPosition?.top !== undefined || rawPosition?.bottom !== undefined) &&
+    (rawPosition?.left !== undefined || rawPosition?.right !== undefined)
+  );
+}
+
+function randInt(max: number) {
   return Math.floor(Math.random() * max);
 }
